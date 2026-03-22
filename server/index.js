4269897ba80a0c89703834774579d2b1039a92b5
ka1tya <kattyavv1@gmail.com>
2026-03-22 19:32:49 +0200

    Update server

---
+++ server/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const users = require("./users.json");
+const data = require("./data.json");
+
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Сервер працює на http://localhost:${PORT}`);
+});
+
+//Обробка запиту на реєстрацію нового користувача
+app.post("/register", (req, res) => {
+  const { name, email, password, phone } = req.body;
+  if (users.find((u) => u.email === email)) {
+    return res.status(400).json({
+      success: false,
+      message: "Користувач з такою поштою вже існує",
+    });
+  }
+
+  // Додавання нового користувача
+  const newUser = {
+    id: String(users.length + 1),
+    name,
+    email,
+    password,
+    phone,
+  };
+
+  users.push(newUser);
+
+  fs.writeFileSync(
+    path.join(__dirname, "users.json"),
+    JSON.stringify(users, null, 2),
+  );
+
+  res.json({
+    success: true,
+    message: "Користувач успішно зареєстрований",
+    user: newUser,
+  });
+});
+
+//Обробка запиту на вхід користувача
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((u) => u.email === email && u.password === password);
+  if (user) {
+    res.json({
+      success: true,
+      message: "Успішний вхід",
+      user,
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      message: "Невірна пошта або пароль",
+    });
+  }
+});
+
+//Обробка запиту на отримання даних
+app.get("/init-data", (req, res) => {
+  res.json(data);
+});
