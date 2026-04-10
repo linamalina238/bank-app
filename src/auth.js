797e5edaa4a4e5e15bfe11149ab77bc1734a0785
DmytroYevmenenko <evmenenkooor@gmail.com>
2026-04-10 17:29:00 +0300

    add auth

---
+++ src/auth.js
@@ -0,0 +1,14 @@
+const API_URL = "http://localhost:3000";
+
+export async function loginUser(email, password) {
+  const response = await fetch(${API_URL}/login, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await response.json();
+  return data;
+}
