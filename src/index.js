5dcfc183fb3166a5371f64cec710d80f6cfb1aa4
piximeow <annapolishchuk2015@gmail.com>
2026-05-04 17:22:06 +0300

    refactor init function to enhance user authentication handling and event subscriptions

--- src/index.js
+++ src/index.js
@@ -9,3 +9,37 @@ import {
   initForms,
 } from "./ui.js";
 
+function init() {
+  const user = getCurrentUser();
+
+  if (user) {
+    showDashboard();
+    renderUserInfo(user);
+  } else {
+    showAuthScreen();
+  }
+
+  initForms();
+
+  eventBus.subscribe("user:login", (user) => {
+    showDashboard();
+    renderUserInfo(user);
+  });
+
+  eventBus.subscribe("user:logout", () => {
+    showAuthScreen();
+  });
+
+  eventBus.subscribe("accounts:updated", (accounts) => {
+    const user = getCurrentUser();
+    if (!user) return;
+    const account = accounts.find((acc) => acc.userId === user.id);
+    if (account) renderBalance(account.balance);
+  });
+
+  eventBus.subscribe("transactions:updated", (transactions) => {
+    renderTransactions(transactions);
+  });
+}
+
+init();
